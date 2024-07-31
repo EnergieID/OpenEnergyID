b42475228d785adcc476cdd67c0d51286b5eb6c8
Jan Pecinovsky <jan.pecinovsky@energieid.be>
2024-07-31 13:50:34 +0000

    Move validation to notebook

--- openenergyid/pandera_poc/analysis.py
+++ openenergyid/pandera_poc/analysis.py
@@ -1,18 +1,11 @@
 """Proof of concept of a data analysis module using pandera."""
 
-from pandera.typing import DataFrame
-from .models import InputModel, OutputModel
+import pandas as pd
 
 
-def analyse(df: DataFrame[InputModel]) -> DataFrame[OutputModel]:
+def analyse(
+    df: pd.DataFrame,
+) -> pd.DataFrame:
     """Perform analysis on the input data and return the output data."""
-    # Validate input data
-    InputModel.validate(df)
-
     # Perform analysis
-    pass
-
-    # Validate output data
-    OutputModel.validate(df)
-
     return df
--- openenergyid/pandera_poc/models.py
+++ openenergyid/pandera_poc/models.py
@@ -2,19 +2,20 @@
 
 import pandera as pa
 
-from pandera.typing import Series
+from pandera.typing import Series, Index
 
 
 class InputModel(pa.DataFrameModel):
     """Pandera schema for input data validation."""
 
-    column1: int = pa.Field(le=10)
-    column2: float = pa.Field(lt=-1.2)
-    column3: str = pa.Field(str_startswith="value_")
+    index: Index[int] = pa.Field(ge=0)
+    column1: Series[int] = pa.Field(le=10)
+    column2: Series[float] = pa.Field(lt=-1.2)
+    column3: Series[str] = pa.Field(str_startswith="value_")
 
     @pa.check("column3")
     @classmethod
-    def column_3_check(cls, series: Series[str]) -> Series[bool]:
+    def column_3_check(cls, series: Series[str]) -> bool:
         """Check that column3 values have two elements after being split with '_'"""
         return series.str.split("_", expand=True).shape[1] == 2
 
@@ -22,13 +23,14 @@ class InputModel(pa.DataFrameModel):
 class OutputModel(pa.DataFrameModel):
     """Pandera schema for output data validation."""
 
-    column1: int = pa.Field(le=10)
-    column2: float = pa.Field(lt=-1.2)
-    column3: str = pa.Field(str_startswith="value_")
+    index: Index[int] = pa.Field(ge=0)
+    column1: Series[int] = pa.Field(le=10)
+    column2: Series[float] = pa.Field(lt=-1.2)
+    column3: Series[str] = pa.Field(str_startswith="value_")
 
     @pa.check("column3")
     @classmethod
-    def column_3_check(cls, series: Series[str]) -> Series[bool]:
+    def column_3_check(cls, series: Series[str]) -> bool:
         """Check that column3 values have two elements after being split with '_'"""
         return series.str.split("_", expand=True).shape[1] == 2
 
@@ -37,4 +39,4 @@ class OutputModel(pa.DataFrameModel):
 
         # Define the output data format
         to_format = "dict"
-        to_format_kwargs = {"orient": "split"}
+        to_format_kwargs = {"orient": "list"}
