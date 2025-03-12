bf0e2736625f1025d002a677ada2d642e8398a09
Jan Pecinovsky <=>
2025-03-12 17:09:54 +0000

    refactoring

--- openenergyid/pvsim/abstract.py
+++ openenergyid/pvsim/abstract.py
@@ -1,9 +1,32 @@
+"""
+This module contains the abstract base class for PVSimulator.
+"""
+
 from abc import ABC, abstractmethod
+from typing import cast
 
 import pandas as pd
 
 
 class PVSimulator(ABC):
+    """
+    An abstract base class for PV simulators.
+    """
+
+    def __init__(self):
+        self._simulation_results: pd.Series | None = None
+
+    @property
+    def simulation_results(self) -> pd.Series:
+        """The results of the simulation."""
+        if self._simulation_results is None:
+            self.simulate()
+            self._simulation_results = cast(pd.Series, self._simulation_results)
+        return self._simulation_results
+
     @abstractmethod
-    def simulate(self, **kwargs) -> pd.DataFrame:
-        pass
+    def simulate(self, **kwargs) -> pd.Series:
+        """
+        Run the simulation and return the results as a Series.
+        """
+        raise NotImplementedError()
--- openenergyid/pvsim/load_factor.py
+++ openenergyid/pvsim/load_factor.py
@@ -1,26 +1,26 @@
-from typing import cast
+"""
+This module contains the LoadFactorPVSimulator class which
+simulates the power output of a PV system based on load factors.
+"""
+
 import pandas as pd
 from .abstract import PVSimulator
 
 
 class LoadFactorPVSimulator(PVSimulator):
-    def __init__(
-        self, load_factors: pd.Series, panel_power: float, inverter_power: float, **kwargs
-    ):
+    """
+    A PV simulator that simulates the power output of a PV system based on load factors.
+    """
+
+    def __init__(self, load_factors: pd.Series, panel_power: float, inverter_power: float):
         self.load_factors = load_factors
         self.panel_power = panel_power
         self.inverter_power = inverter_power
 
-        self._simulation_results: pd.Series | None = None
-
-    @property
-    def simulation_results(self) -> pd.Series:
-        if self._simulation_results is None:
-            self.simulate()
-            self._simulation_results = cast(pd.Series, self._simulation_results)
-        return self._simulation_results
+        super().__init__()
 
     def simulate(self, **kwargs) -> pd.Series:
+        """Run the simulation."""
         result = self.load_factors * self.panel_power * 0.01
         result.clip(upper=self.inverter_power, inplace=True)
         result.rename("power", inplace=True)
