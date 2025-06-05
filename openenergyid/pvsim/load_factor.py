a8431e0af6c572d0790b9f27fd8679b1f7968c83
Jan Pecinovsky <=>
2025-06-05 08:32:35 +0000

    Add asynchronous method to download load factors from API
    - Introduced `download_load_factors` static method in `LoadFactorPVSimulator` class.
    - Utilizes `aiohttp` for asynchronous HTTP requests to fetch load factors.
    - Parses the response using `elia` module to return a pandas Series of load factors.

--- openenergyid/pvsim/load_factor.py
+++ openenergyid/pvsim/load_factor.py
@@ -3,8 +3,11 @@ This module contains the LoadFactorPVSimulator class which
 simulates the power output of a PV system based on load factors.
 """
 
+import aiohttp
 import pandas as pd
 from .abstract import PVSimulator
+from openenergyid import elia
+import datetime as dt
 
 
 class LoadFactorPVSimulator(PVSimulator):
@@ -26,3 +29,24 @@ class LoadFactorPVSimulator(PVSimulator):
         result.rename("power", inplace=True)
         self._simulation_results = result
         return result
+
+    @staticmethod
+    async def download_load_factors(
+        start: dt.date,
+        end: dt.date,
+        region: elia.Region,
+        session: aiohttp.ClientSession,
+    ) -> pd.Series:
+        """Download load factors from the API."""
+        pv_load_factor_json = await elia.get_dataset(
+            dataset="ods032",
+            start=start,
+            end=end,
+            region=region,
+            select={"loadfactor"},
+            session=session,
+        )
+        pv_load_factors = elia.parse_response(
+            data=pv_load_factor_json, index="datetime", columns=["loadfactor"]
+        )["loadfactor"]
+        return pv_load_factors
