bbc638e74b6a5f7bec51ecbc1680a8448897392e
Oscar <7408635+Molier@users.noreply.github.com>
2026-01-19 16:18:44 +0100

    Enhance Baseload Analysis, Update Release Workflow, and Documentation (#32)

--- openenergyid/baseload/models.py
+++ openenergyid/baseload/models.py
@@ -30,3 +30,10 @@ class BaseloadResultSchema(pa.DataFrameModel):
     consumption_not_due_to_baseload_in_kilowatthour: float
     baseload_ratio: float = pa.Field(ge=0, le=2)
     consumption_due_to_median_baseload_in_kilowatthour: float = pa.Field(ge=0)
+
+
+class MonthlyMedianBaseloadSchema(pa.DataFrameModel):
+    """Validates monthly median baseload results"""
+
+    timestamp: DateTime = pa.Field()
+    monthly_median_baseload_in_watt: float = pa.Field(ge=0)
